8f3d73e90b1d720db27586203b7b3e4c4afe9e4f
Nicholas R Sajka <nicksajka@gmail.com>
2017-07-26 22:29:10 -0400

    Final commit of rebuilt repo due to Heroku issues

---
+++ app/data/friends.js
@@ -0,0 +1,16 @@
+var friends = [ {
+	"name" : "Benji",
+	"photo" : "https://vetstreet-brightspot.s3.amazonaws.com/6c/c9/03f60ba14dc1bfeddce2ff9c064c/golden-retriever-AP-0YXM8S-645sm8513.jpg",
+	"scores" : [5,5,2,3,1,5,2,1,3,5]
+}, {
+	"name" : "Carl",
+	"photo" : "https://s-media-cache-ak0.pinimg.com/originals/aa/70/2e/aa702e0155e11498c9dfb6ac18c9e924.jpg",
+	"scores" : [1,5,1,1,1,1,1,1,5,5]
+}, {
+	"name" : "Lou",
+	"photo" : "http://static.flickr.com/34/122530930_6e16f1eb5c.jpg",
+	"scores" : [5,3,2,3,2,5,1,3,3,4]
+}];
+
+
+module.exports = friends;
